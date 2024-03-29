810aafa5a8e79d7e124f15faf9481f88f8d50a7f
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-03-29 16:52:23 -0700

    A function that returns the value of the last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        i = 0
+        while number < 0:
+            i = i+1
+            number = number+1
+        number = i
+        
+    ld = number % 10
+    print("{}".format(ld), end="")
+    return ld
