a063bea26c686f4fdb8ab789037c36620362065c
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-03-29 17:06:43 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,12 +3,12 @@ i = 0
 while i < 10:
     j = 0
     while j < 10:
-        if i == j or i >  j:
+        if i == j or i > j:
             j = j+1
             continue
         if i == 8 and j == 9:
-            print("{:d}{:d}".format(i,j))
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{:d}{:d},".format(i,j), end=" ")
+            print("{:d}{:d},".format(i, j), end=" ")
         j = j+1
     i = i+1
