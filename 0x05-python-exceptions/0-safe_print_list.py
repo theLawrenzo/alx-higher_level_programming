f362471fa5bb9df78c502eab5fa6637bf0e0a4a3
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-04-03 20:21:04 -0700

    UPDATE

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while True:
+        try:
+            for i in my_list:
+                print("{}".format(i), end="")
+                i += 1
+
+        except Exception:
+            print('An error occured!')
+    return i
