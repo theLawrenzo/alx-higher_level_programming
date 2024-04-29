82b65ae3f69f3de70df991176f8d9f9857e0c65c
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-04-29 16:15:21 -0700

    PROGRAM UPDATE: [2-args.py] minor little change to enhance program performance

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 import sys
-num = len(sys.argv)
-if num == 0:
-    print(
+
+def print_args():
+    num = len(sys.argv) - 1
+    if num == 0:
+        print(f"{num} arguments.")
+    elif num == 1:
+        print(f"{num} argument:")
+        print(f"{num}: {sys.argv[num]}")
+    elif num > 1:
+        print(f"{num} arguments:")
+        i = 1
+        while i <= num:
+            print(f"{i}: {sys.argv[i]}")
+            i += 1
+
+if __name__ == "__main__":
+    print_args()
