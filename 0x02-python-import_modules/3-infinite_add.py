0aff7f5df192368802b044b18204824449e74f1e
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-04-30 15:24:36 -0700

    MODULE: [3-infinite_add.py] Program that adds all the integer that it receives as input

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def infinite_add():
+    num = len(sys.argv) - 1
+    arg = 0
+    if num == 0:
+        print('0')
+    elif num == 1:
+        print(sys.argv[1])
+    elif num > 1:
+        i = 1
+        while i <= num:
+            arg += int(sys.argv[i])
+            i += 1
+    print(arg)
+
+if __name__ == "__main__":
+    infinite_add()
