6ac3eb07f0620541337047e6435d066397326399
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-04-30 15:44:10 -0700

    UPDATED MODULE [3-inifinite_add.py] : minor update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-def infinite_add():
     num = len(sys.argv) - 1
     arg = 0
     if num == 1:
@@ -12,6 +12,3 @@ def infinite_add():
             arg += int(sys.argv[i])
             i += 1
     print(arg)
-
-if __name__ == "__main__":
-    infinite_add()
