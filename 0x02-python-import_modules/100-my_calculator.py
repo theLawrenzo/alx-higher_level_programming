66acbc0caca858d39279ea6746738bf135f84393
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-07-31 18:22:06 -0700

    My mini python calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+import sys
+import calculator_1
+
+if __name__ == '__main__':
+    length = len(sys.argv)
+    if length < 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sys.argv[2] == '+':
+        c = calculator_1.add(a, b)
+        print('{0} + {1} = {2}'.format(a, b, c))
+    elif sys.argv[2] == '-':
+        c = calculator_1.sub(a, b)
+        print('{0} - {1} = {2}'.format(a, b, c))
+    elif sys.argv[2] == '*':
+        c = calculator_1.mul(a, b)
+        print('{0} * {1} = {2}'.format(a, b, c))
+    elif sys.argv[2] == '/':
+        c = calculator_1.div(a, b)
+        print('{0} / {1} = {2}'.format(a, b, c))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
