da74476910d4702b94781456477df0ce661ae2e8
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-04 16:14:18 -0700

    Upadated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    l = len(my_list) - 1
-    if idx < 0 or idx > l:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list[:]
     else:
         new_list = my_list[:]
