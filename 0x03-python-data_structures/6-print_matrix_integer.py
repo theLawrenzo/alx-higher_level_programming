5a25ad1fb374b5e10acb95bc42abbfc76c96b2c8
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-05 15:42:52 -0700

    Python script that contains a function that prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in row:
-            print('{:d}'.format(i), end=" ")
+    i = 0
+    while i < len(matrix):
+        j = 0
+        row = matrix[i]
+        while j < len(row):
+            if j == len(row) - 1:
+                print('{:d}'.format(j), end="")
+            else:
+                print('{:d}'.format(j), end=" ")
+            j += 1
         print()
+        i += 1
