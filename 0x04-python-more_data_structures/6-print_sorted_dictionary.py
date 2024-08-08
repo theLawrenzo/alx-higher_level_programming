82918974432f9c7588d26d36adce0c136155d684
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-08 15:57:07 -0700

    PYTHON SCRIPT - Contains function that prints a sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,9 @@ def print_sorted_dictionary(a_dictionary):
     list_keys = list(dict.keys(a_dictionary))
     list_keys.sort()
     j = 0
+
     for i in a_dictionary:
         print("%s: %s" %(list_keys[j], a_dictionary[list_keys[j]]))
         j = j + 1
+    
     return None
