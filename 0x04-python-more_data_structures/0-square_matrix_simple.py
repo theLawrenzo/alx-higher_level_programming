51479f7f891e2fea4fd5288a8a0c9ed9e9b4791a
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-13 20:46:24 -0700

    PYTHON SCRIPT - Function that computes and returns the squares of all integers in a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [row for row in matrix for i in row]
+    new_matrix = []
+
+    if len(matrix) != 0:
+        for m in matrix:
+            new_matrix.append([i ** 2 for i in m])
+
     return new_matrix
