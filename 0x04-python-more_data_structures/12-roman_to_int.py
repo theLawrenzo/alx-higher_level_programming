19f47cd6b94bb1a10f56743553bdf5f6970800f2
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-21 17:35:19 -0700

    Update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,\
-            'C': 100, 'D': 500, 'M': 1000, 'MMMCMXCIX': 3999}
+            'C': 100, 'D': 500, 'M': 1000}
 
     length = len(roman_string)
     val = 0
