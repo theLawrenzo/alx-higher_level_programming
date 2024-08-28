7f88c94b0ba1beda276707ebcfc240990337cc4a
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-27 20:22:30 -0700

    PYTHON SCRIPT - Contains function that returns a weighted average of a list of tuples

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/python3
+def weight_average(my_list=[]):
+    if isinstance(my_list, list) and len(my_list):
+        score = 0
+        weight = 0
+        for x in my_list:
+            score += (x[0] * x[1])
+            weight += (x[1])
+        return (score / weight)
+    return 0
