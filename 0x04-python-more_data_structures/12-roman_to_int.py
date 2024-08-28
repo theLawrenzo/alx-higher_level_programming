2518dbbdcf75031577da27f4ac3ccd60820c2a24
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-08-27 20:11:05 -0700

    Update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,26 +2,20 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,\
             'C': 100, 'D': 500, 'M': 1000}
     length = len(roman_string)
     val = 0
     i = 0
-
     while i < length:
         if i == length-1:
             val += roman_numerals[roman_string[i]]
             return val
-
         first = roman_numerals[roman_string[i]]
         second = roman_numerals[roman_string[i+1]]
-        
         if first < second:
             val -= first
         else:
             val += first
-
         i += 1
-
     return val
